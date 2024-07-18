cee910d6d39b7288832621f187150f58e63d6e75
Ken Lippold <klippold@Kens-MacBook-Pro-2.local>
2024-07-18 16:25:29 -0700

    Updated example project secret key value.

--- example/example/settings.py
+++ example/example/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bfr86m@*qy2sro(v6qhb9fn2fgtyce@f_2ofvd_t(!3y5on%#s'
+SECRET_KEY = 'django-insecure-bfr86m@*qy2sro(v6qhb9fn2fgtyce@f_2ofvd_t(!3y5on%#s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
